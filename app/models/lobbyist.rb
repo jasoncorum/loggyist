0a3e2a92957f5bd8f1ece23141d16c210dd4be8d
Jason Corum <jason.corum@gmail.com>
2014-08-23 15:32:23 -0400

    Added SessionController for Admins

--- app/models/lobbyist.rb
+++ app/models/lobbyist.rb
@@ -1,4 +1,7 @@
 class Lobbyist < ActiveRecord::Base
+  
+  belongs_to :admin, inverse_of: :lobbyist
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :admins
+  
+  devise_for :admins, controllers: { sessions: "admins/sessions" }  
   devise_for :lobbyists
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
