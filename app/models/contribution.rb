f9e9ca19cea70273907d4a4f2a0c33fcd09fa835
Jason Corum <jason.corum@gmail.com>
2014-08-30 16:00:47 -0400

    Edits to Contrbution index and model to handle no notes

--- app/models/contribution.rb
+++ app/models/contribution.rb
@@ -1,8 +1,17 @@
 class Contribution < ActiveRecord::Base
 
+	belongs_to :lobbyist, inverse_of: :contributions
+	
 	validates :recipient, :date, presence: true
 	validates :amount, numericality: { only_integer: true }
 
-	belongs_to :lobbyist, inverse_of: :contributions
+	NULL_ATTRS = %w( notes )
+  before_save :nil_if_blank
+
+  private
+
+  def nil_if_blank
+  	NULL_ATTRS.each { |attr| self[attr] = nil if self[attr].blank? }
+  end
 
 end
