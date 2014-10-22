4f2cfa2c354ca677dcd1f4d07ddb5364e5a2a9f2
Jason Corum <jason.corum@gmail.com>
2014-10-22 19:01:36 -0400

    Created report view for Contribution and added to site nav

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,7 @@ Rails.application.routes.draw do
   #   get 'products/:id' => 'catalog#view'
 
   get 'activities/report' => 'activities#report'
+  get 'contributions/report' => 'contributions#report'
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
