b4b91410ada29f5f8ce098a4c267d4f9bab4120c
Jason Corum <jason.corum@gmail.com>
2014-10-23 21:29:19 -0400

    Applied datepicker to activities reporting but search broke. Need to convert text_field data to date data?

--- app/assets/javascripts/reporting.js
+++ app/assets/javascripts/reporting.js
@@ -1,6 +1,10 @@
-if (!Modernizr.inputtypes.date) {
-    $('input[type=date]').datepicker({
-        // Consistent format with the HTML5 picker
-        dateFormat: 'yy-mm-dd'
-    });
-};
+// if (!Modernizr.inputtypes.date) {
+//     $('input[type=date]').datepicker({
+//         // Consistent format with the HTML5 picker
+//         dateFormat: 'yy-mm-dd'
+//     });
+// };
+$(function (){
+  $('#q_').datepicker();
+  $('#q2_').datepicker();
+});
