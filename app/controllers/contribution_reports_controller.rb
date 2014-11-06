da5f538206f23681f4d93c84cae9aced6a529614
Jason Corum <jason.corum@gmail.com>
2014-11-06 16:48:27 -0500

    Breaking sh*t to learn how to create reports.

--- app/controllers/contribution_reports_controller.rb
+++ app/controllers/contribution_reports_controller.rb
@@ -1,6 +1,31 @@
 class ContributionReportsController < ApplicationController
 
-	def index
+	def new
+		@contribution_report = current_lobbyist.contribution_reports.contribution_search(params[:from], params[:to]).build
+		@lobbyist_contributions = current_lobbyist.contributions.all.reverse
+		
+		# report_start_date = build_date_from_params("start_date", params[:report])
+		# report_end_date = build_date_from_params("end_date", params[:report])
 	end
+
+	def create
+    if @contribution_report.save
+			redirect_to @contribution_report
+		else
+			render 'new'
+		end
+  end
+
+  def show
+  	@contribution_report = ContributionReport.find params[:id]
+  end
+
+	private
+
+	# def build_date_from_params(field_name, params)
+ #  	Date.new(params["report_#{field_name}_1i"].to_i, 
+ #       params["report_#{field_name}_2i"].to_i, 
+ #       params["report_#{field_name}_3i"].to_i)
+	# end
 
 end
--- app/models/contribution_report.rb
+++ app/models/contribution_report.rb
@@ -3,5 +3,10 @@ class ContributionReport < ActiveRecord::Base
 	has_and_belongs_to_many :contributions
 	belongs_to :lobbyist, inverse_of: :contribution_reports, dependent: :destroy
 
+	def self.contribution_search(param, param2)
+		Contribution.find(:all, :conditions => ['date >= :query and date <= :query2',
+    query: param, query2: param2]
+  end
+
 end
 
